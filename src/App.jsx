ee53600e34fb33ee09b7245d0de22b4b9bc65772
Arindam1203 <mailsoothers@gmail.com>
2026-03-22 23:01:45 +0530

    RISHI first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <iframe
+      src="/landing.html"
+      style={{ width:'100vw', height:'100vh', border:'none' }}
+      title="RISHI Landing"
+    />
+  )
+}
